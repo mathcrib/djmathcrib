c8b2e44e445af590f96bad3e0f7411b5b068787d
jlllk <ehyf.py@gmail.com>
2020-08-26 23:53:43 +0300

    Formatted code

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+
 from django_filters.views import FilterView
 
 from .filters import ArticleFilter
-from .views import (ArticleCreateView, ArticleDetailView, ArticleListView,
-                    ArticleUpdateView)
+from .views import (
+    ArticleCreateView,
+    ArticleDetailView,
+    ArticleListView,
+    ArticleUpdateView
+)
 
 urlpatterns = [
     path('<int:pk>/read/', ArticleDetailView.as_view(), name='article_detail'),
@@ -14,5 +19,9 @@ urlpatterns = [
         name='article_update',
     ),
     path('', ArticleListView.as_view(), name='article_list'),
-    path('search/', FilterView.as_view(filterset_class=ArticleFilter), name='search'),
+    path(
+        'search/',
+        FilterView.as_view(filterset_class=ArticleFilter),
+        name='search',
+    ),
 ]
--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 
-from .models import User, InvitedUser
+from .models import InvitedUser, User
 
 
 class CustomUserAdmin(BaseUserAdmin):
