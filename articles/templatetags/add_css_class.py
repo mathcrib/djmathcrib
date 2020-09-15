7d9ca275787668c07f63dadc5b1da44b23410a9b
jlllk <ehyf.py@gmail.com>
2020-09-15 23:11:38 +0300

    Updated password reset templates

--- articles/templatetags/add_css_class.py
+++ articles/templatetags/add_css_class.py
@@ -7,6 +7,7 @@ register = template.Library()
 def addclass(field, css):
     return field.as_widget(attrs={"class": css})
 
+
 @register.filter
 def addsuffix(field, suffix):
     return field.label_tag(label_suffix=suffix)
